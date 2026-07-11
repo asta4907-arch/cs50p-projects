50050ae44ae11f13c92b93016094704f9f3ffdc0
asta4907-arch <asta4907@gmail.com>
2026-07-11 21:36:31 +0530

    nutition facts

---
+++ nutrition/nutrition.py
@@ -0,0 +1,16 @@
+item = input("Item: ").title()
+
+if item == "Apple":
+    print("Calorie: 130")
+elif item == "Banana":
+    print("Calorie: 110")
+elif item == "Sweet Cherries":
+    print("Calorie: 100")
+elif item == "Avocado":
+    print("Calorie: 50")
+elif item == "Kiwifruit":
+    print("Calorie: 90")
+elif item == "Pear":
+    print("Calorie: 100")
+else:
+    print("")
